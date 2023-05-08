815b3f1afcf553ce3c394dd1f5ce45b74c9f3d2e
Kesco Lin <kesco915@gmail.com>
2023-05-08 23:12:14 +0800

    Added: Telescop show dianostics

--- mappings.lua
+++ mappings.lua
@@ -4,8 +4,9 @@ local M = {}
 M.disabled = {
   n = {
     ["<leader>b"] = "",
+    ["<leader>f"] = "",
     ["<leader>fb"] = "",
-  }
+  },
 }
 
 M.general = {
@@ -13,7 +14,7 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>bb"] = { "<cmd> enew <CR>", "new buffer" },
     ["[q"] = { "<cmd> cprev <CR> zz", "previous quickFix" },
-    ["]q"] = { "<cmd> cnext <CR> zz", "next quickFix" }
+    ["]q"] = { "<cmd> cnext <CR> zz", "next quickFix" },
   },
 }
 
@@ -21,8 +22,9 @@ M.telescope = {
   n = {
     ["<leader>bl"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
     ["<leader>ss"] = { "<cmd> Telescope lsp_document_symbols <CR>", "document symbols" },
-    ["<leader>sd"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "workspace symbols" }
-  }
+    ["<leader>sd"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "workspace symbols" },
+    ["<leader>ld"] = { "<cmd> Telescope diagnostics <CR>", "workspace diagnostics" },
+  },
 }
 
 return M
